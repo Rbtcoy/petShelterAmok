e2cbd19588a7c566f5bf98909be75198c831d824
Rbtcoy <rbtcy@outlook.com>
2023-07-11 11:58:41 -0400

    starting to write game interface

--- app/src/main/java/petshelteramok/SystemLogic.java
+++ app/src/main/java/petshelteramok/SystemLogic.java
@@ -2,15 +2,20 @@ package petshelteramok;
 
 public class SystemLogic {
 
-    public void promptUser() {
-        // will adjust these later to actual options
-        System.out.println("What would you like to do now? \n");
-        System.out.println("1. Feed them.");
-        System.out.println("2. Give them water.");
-        System.out.println("3. Play with a pet");
-        System.out.println("4. Adopt a pet (remove from menu)");
-        System.out.println("5. Admit a pet (add a pet to menu)");
-        System.out.println("6. Quit T~T \n");
-    }
+    // public void promptUser() {
+    //     // will adjust these later to actual options
+    //     System.out.println("What would you like to do now? \n");
+    //     System.out.println("1. Feed them.");
+    //     System.out.println("2. Give them water.");
+    //     System.out.println("3. Play with a pet");
+    //     System.out.println("4. Adopt a pet (remove from menu)");
+    //     System.out.println("5. Admit a pet (add a pet to menu)");
+    //     System.out.println("6. Quit T~T \n");
+    // }
+    //depricated, need to adjust for new pets
 
+    public void initMenu(){
+        System.out.println("What would you like to do today?");
+        
+    }
 }
